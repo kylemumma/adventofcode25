0c2e7fd886fa6c51c6caeeab2fcaaa45fc4c4014
Kyle Mumma <kyle.mumma@sentry.io>
2025-12-08 12:04:21 -0800

    day 2 complete

--- rs/src/day2/day2.rs
+++ rs/src/day2/day2.rs
@@ -11,8 +11,10 @@ fn get_divisors(n: u64) -> Vec<u64> {
     let mut divisors = Vec::new();
     for i in 1..=n.isqrt() {
         if n % i == 0 {
-            divisors.push(i);
-            if i != (n/i) {
+            if i <= n/2 {
+                divisors.push(i);
+            }
+            if i != (n/i) && (n/i) <= n/2 {
                 divisors.push(n/i);
             }
         }        
@@ -35,17 +37,29 @@ fn is_made_of(s: &str, subs: &str) -> bool {
 }
 
 pub fn run() {
+    //println!("{}", is_made_of("565656", "56"));
+    //return;
     let mut ctr: u64 = 0;
     let mut newctr: u64 = 0;
-    for item in read_file("src/day2/sample.txt") {
+    for item in read_file("src/day2/input.txt") {
         let parts: Vec<u64> = item.split('-').map(|e| e.parse().unwrap()).collect();
-//        println!("will check {} to {}", parts[0], parts[1]);
+        println!("{}-{}", parts[0], parts[1]);
         for i in parts[0]..(parts[1]+1) {
+        //for i in 565656..565657 {
             let istr = i.to_string();
             let divisors = get_divisors(istr.len() as u64);
+            let div2 = divisors.clone();
+            
+            //println!("{}: {} divisors {:?}", istr, istr.len(), div2);
             let mut found = false;
             for d in divisors {
-                if is_made_of(&istr, &istr[0..(d as usize)]) {
+                let arg1 = &istr;
+                let arg2 = &istr[0..(d as usize)];
+                //println!("{}, {}", arg1, arg2);
+                let res = is_made_of(arg1, arg2);
+                if res {
+                    
+                    println!("{:?} is made of {:?}",arg1,arg2);
                     found = true;
                     break;
                 }
@@ -53,19 +67,8 @@ pub fn run() {
             if found {
                 newctr += i;
             }
-            // old code:
-            let chars: Vec<char> = i.to_string().chars().collect();            
-            if chars.len() % 2 != 0 {
-                continue;
-            }
-            let mid = chars.len() / 2;
-            if &chars[..mid] == &chars[mid..] {
-                ctr += i;
-//                println!("{:?}", chars);
-            }
-
         }
+        println!();
     }
-    println!("{}", ctr);
     println!("{}", newctr);
 }
