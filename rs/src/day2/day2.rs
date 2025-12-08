586df24d5a9bd3947a2aeaef12d1666e1faedcef
Kyle Mumma <kyle.mumma@sentry.io>
2025-12-08 11:33:57 -0800

    dsfhjsdkdjfh

--- rs/src/day2/day2.rs
+++ rs/src/day2/day2.rs
@@ -7,13 +7,54 @@ pub fn read_file(path: &str) -> impl Iterator<Item = String> {
     reader.split(b',').map(|e| String::from_utf8(e.unwrap()).unwrap().trim().to_string())
 }
 
+fn get_divisors(n: u64) -> Vec<u64> {
+    let mut divisors = Vec::new();
+    for i in 1..=n.isqrt() {
+        if n % i == 0 {
+            divisors.push(i);
+            if i != (n/i) {
+                divisors.push(n/i);
+            }
+        }        
+    }
+    divisors
+}
+
+fn is_made_of(s: &str, subs: &str) -> bool {
+    // assuming utf-8 strings that are size divisible
+    let mut start = 0;
+    let mut end = subs.len();
+    while start < s.len() {
+        if &s[start..end] != subs {
+            return false;
+        }
+        start = end;
+        end = start + subs.len();
+    }
+    true
+}
+
 pub fn run() {
     let mut ctr: u64 = 0;
-    for item in read_file("src/day2/input.txt") {
+    let mut newctr: u64 = 0;
+    for item in read_file("src/day2/sample.txt") {
         let parts: Vec<u64> = item.split('-').map(|e| e.parse().unwrap()).collect();
 //        println!("will check {} to {}", parts[0], parts[1]);
         for i in parts[0]..(parts[1]+1) {
-            let chars: Vec<char> = i.to_string().chars().collect();
+            let istr = i.to_string();
+            let divisors = get_divisors(istr.len() as u64);
+            let mut found = false;
+            for d in divisors {
+                if is_made_of(&istr, &istr[0..(d as usize)]) {
+                    found = true;
+                    break;
+                }
+            }
+            if found {
+                newctr += i;
+            }
+            // old code:
+            let chars: Vec<char> = i.to_string().chars().collect();            
             if chars.len() % 2 != 0 {
                 continue;
             }
@@ -26,4 +67,5 @@ pub fn run() {
         }
     }
     println!("{}", ctr);
+    println!("{}", newctr);
 }
